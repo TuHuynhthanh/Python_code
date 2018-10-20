5cb094b48d5d1c522c91cd05e7428ff39a52d925
tuhuynthanh <tu.huynthanh@stu.edu.vn>
2018-10-20 12:28:45 +0700

    add backgroundsubstractor file

---
+++ backgroundsubstractorMOG_ex1.py
@@ -0,0 +1,15 @@
+import numpy as np
+import cv2 as cv
+cap = cv.VideoCapture('./data/car.avi')
+fgbg = cv.bgsegm.createBackgroundSubtractorMOG()
+
+while(1):
+    ret, frame = cap.read()
+    fgmask = fgbg.apply(frame)
+    cv.imshow('frame',fgmask)
+    cv.imshow('video',frame)
+    k = cv.waitKey(30) & 0xff
+    if k == 27:
+        break
+cap.release()
+cv.destroyAllWindows()
